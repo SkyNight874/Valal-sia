75fd41f0d9faca65d553c177535881f36b75a0f4
SkyNight874 <skyfo@LAPTOP-6U8O48JN>
2021-06-14 18:15:42 +0200

    Added the Venom Puddle and the Living Flame

---
+++ src/main/java/net/mcreator/valalsiamod/ClientProxyValalsiaModMod.java
@@ -0,0 +1,26 @@
+package net.mcreator.valalsiamod;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.obj.OBJLoader;
+
+public class ClientProxyValalsiaModMod implements IProxyValalsiaModMod {
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		OBJLoader.INSTANCE.addDomain("valalsia_mod");
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
